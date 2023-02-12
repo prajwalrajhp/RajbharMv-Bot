81fffee08dfab38be62d0e2430722a087584e38b
CrezyDeveloper <122361875+CrezyDeveloper@users.noreply.github.com>
2023-02-12 09:12:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ MSG_ALRT = environ.get('MSG_ALRT', 'Share and Support Us')
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', -1001856507861))
 FILE_CHANNEL = int(environ.get('FILE_CHANNEL',  -1001874085827))
 FILE_CHANNEL_LINK = environ.get('FILE_CHANNEL_LINK', '')
-HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://t.me/dulink_file_open')
+HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://youtu.be/P52W-ZXXyio')
 
 # Log Channels
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', -1001634572825))
