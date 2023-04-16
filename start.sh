087b9c2818152e96bce0b2d648d371f32ea6e0a6
TECHNICAL RB <121280660+TECHNICAL-RB@users.noreply.github.com>
2023-04-16 21:10:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/CrezyDeveloper143/Autofilterbot-.git /Autofilterbot-
+  git clone https://github.com/TECHNICAL-RB/Autofilter-bot-cynite.git /Autofilter-bot-cynite
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-Filter-V5
+  git clone $UPSTREAM_REPO /Autofilter-bot-cynite
 fi
-cd /Autofilterbot-
+cd /Autofilter-bot-cynite
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
